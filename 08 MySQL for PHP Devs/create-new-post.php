ba4ee3588e978914181005e8e91d0e73e88ba3a4
Anna <annsta@gmail.com>
2016-07-11 00:00:35 +0300

    MySQL for PHP Devs

---
+++ 08 MySQL for PHP Devs/create-new-post.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create New Post in MySQL</title>
+</head>
+<body>
+
+<form>
+    <div>Title</div>
+    <input type="text" name="title">
+    <div>Content</div>
+    <textarea name="content"></textarea>
+    <div><input type="submit" value="Post"></div>
+</form>
+
+<?php
+if (isset($_GET['title'])) {
+    $mysqli = new mysqli('localhost', 'root', '', 'blog');
+    $mysqli->set_charset("utf8");
+
+    $stmt = $mysqli->prepare("INSERT INTO posts(title, content) VALUES (?, ?)");
+    $title = htmlspecialchars($_GET['title']);
+    $content = htmlspecialchars($_GET['content']);
+    $stmt->bind_param("ss", $title, $content);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 1) echo "Post created.";
+    else die("Insert post failed.");
+}
+?>
+
+</body>
+</html>
---
+++ 08 MySQL for PHP Devs/delete-post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Post from MySQL</title>
+</head>
+<body>
+
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'blog');
+$mysqli->set_charset("utf8");
+
+if (isset($_GET['id'])) {
+    $st = $mysqli->prepare("DELETE FROM posts WHERE id = ?");
+    $id = htmlspecialchars($_GET['id']);
+    $st->bind_param("i", $id);
+    $st->execute();
+    
+    if ($st->affected_rows == 1) echo "Post deleted.";
+}
+$result = $mysqli->query('SELECT id, title FROM posts');
+while ($row = $result->fetch_assoc()) {
+    $title = htmlspecialchars($row['title']);
+    $delLink = 'delete-post.php?id=' . $row['id'];
+    echo "<p><a href='$delLink'>Delete post '$title'.</a></p>";
+}
+?>
+
+</body>
+</html>
