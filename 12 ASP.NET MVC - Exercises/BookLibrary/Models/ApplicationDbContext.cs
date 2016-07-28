9c41194b2bb5f629f9fe913b7efafce6a9e81e83
Anna <annsta@gmail.com>
2016-07-28 03:56:13 +0300

    ASP.NET MVC

---
+++ 12 ASP.NET MVC - Exercises/BookLibrary/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ 14 Java Basics/Java-Basics-Lab/src/com/company/HelloJava.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
