d4d239731b7aabba1d43bdd2e4565fe64bee4a50
Anna <annsta@gmail.com>
2016-08-12 01:53:52 +0300

    Java Basics

--- 14 Java Basics - Exercises/src/P26IntersectionOfCircles.java
+++ 14 Java Basics - Exercises/src/P26IntersectionOfCircles.java
@@ -22,4 +22,39 @@ public class P26IntersectionOfCircles {
 
 //    use to submit in Judge
 
+    static class Circle {
+        private int x;
+        private int y;
+        private int radius;
+
+        public Circle(int x, int y, int radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+
+        public void setRadius(int radius) {
+            this.radius = radius;
+        }
+    }
 }
